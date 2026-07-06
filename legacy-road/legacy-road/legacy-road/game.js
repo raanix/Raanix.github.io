a06716b129590e85e270d4e46f097581053956c9
raanix <sebitstudio@gmail.com>
2026-07-06 16:54:56 +0300

    Add basic game setup and alert functions

---
+++ legacy-road/legacy-road/legacy-road/game.js
@@ -0,0 +1,12 @@
+const canvas = document.createElement('canvas');
+canvas.width = 1000;
+canvas.height = 600;
+
+// Simple alert for now
+function showSettings() {
+  alert("Audio Settings\nVolume: 75% (More options coming soon)");
+}
+
+function downloadGame() {
+  alert("📥 Download feature coming after we add more files.\nYou can right-click and save the page for now.");
+}
